18230ec0a5946827fc8a800b42b0c761e329abba
Vladislav Yuskiv <yuskiv280478@gmail.com>
2022-01-22 11:49:24 +0200

    Created functions for working with contacts.json and correct work all requests

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,68 @@
 const express = require('express')
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+  updateContact
+ 
+} = require('../../models/contacts');
 
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+   listContacts().then(contacts => {
+      const parseContacts = JSON.parse(contacts)
+        res.status(200).json({ contacts : parseContacts})
+     })
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+ 
+  getContactById(req.params.contactId).then(searchContact => {
+    if (searchContact) {
+      res.status(200).json({ searchContact })
+    }
+    else {
+      res.status(404).json({ message : "Not found"})
+    }
+  })
+  
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+
+  addContact(req.body).then(contact => {
+       res.status(201).json({ status: 'success' , contact })
+  })
+  
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+
+  removeContact(req.params.contactId).then(delatedContact => {
+    if (delatedContact) {
+      res.status(200).json({ message: 'contact deleted' })
+    }
+    else {
+       res.status(404).json({ message: 'Not found' })
+    }
+  })
+
+  
+
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  updateContact(req.params.contactId, req.body).then(updatedContact => {
+    if (updatedContact) {
+      res.status(200).json({ message: 'contact updated' , updatedContact })
+    }
+    else {
+       res.status(404).json({ message: 'Not found' })
+    }
+  })
+
 })
 
 module.exports = router
